ac03a078ef5fc4c45a4c77860b125c7dc5b0b374
Ignacio Coloma <icoloma@extrema-sistemas.com>
2012-06-05 10:04:43 +0200

    Added automatic inclusion of CSS file Added instructions of use

--- README.md
+++ README.md
@@ -1,4 +1,84 @@
 koliseo-widgets
 ===============
 
-A collection of widgets that make use of the Koliseo API
+A collection of widgets that use the Koliseo API.
+
+KPerf
+=====
+
+KPerf displays the scheduled performances for the specified show. To use, just add the following at the end of your web page (before the ending <code>&lt;/body></code> tag):
+
+```html
+<!-- KPerf requires jQuery. If your web page already has jQuery present, you may omit this first line. -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="http://www.koliseo.com/w/kperf/1.0/kperf.min.js"></script>
+```
+
+KPerf will display the widget inside any container with <code>class=kcontainer</code>. For example:
+
+```html
+<aside class="kcontainer" data-show-id="Dfe"></aside>
+```
+
+Parameters
+==========
+
+KPerf can receive any parameters as <code>data-*</code> attributes in the container DOM element. The supported attributes are:
+
+* **showId** (string) the show ID, extracted from the show URL <code>/shows/{showId}/{showName}</code>. This attribute is required.
+* **locale** (string) the two-letter acronym of the locale to use. The default is the browser locale or <code>en</code> if none.
+* **dateFormat** (string) the date format to use. The default is locale-specific.
+* **maxEntries** (number) the maximum number of entries to display. The default is 5.
+* **insertCss** (boolean) By default kperf will insert automatically kperf.css if it is not already present. Set this to false if you plan to provide your own styles somewhere else.
+
+Only showId is required. An example using all parameters:
+
+```html
+<div class="kcontainer" 
+	data-show-id="Dfe" 
+	data-locale="es" 
+	data-date-format="Ddd Mm* d*" 
+	data-max-entries="10" 
+	data-insert-css="false">
+</div>
+```
+
+Using KPerf as a jQuery plugin
+==============================
+
+KPerf is a jQuery plugin that can also be initialized using standard JavaScript syntax:
+
+```javascript
+$('#my-container').kperf({
+	showId: 'xDE5',
+	locale: 'es',
+	dateFormat: $('#my-container').width() < 200? 'dd/mm' : 'Ddd Mm* d*'
+});
+```
+
+Locales
+=======
+
+The widget locale is automatically set to the browser locale, unless otherwise specified using the <code>locale</code> option. The widget supports the same set of locales as the Koliseo website, and will default to <code>en</code> if a locale is not supported.
+
+You can add new locales by setting a global Koliseo.resources variable before including <code>kperf.js</code>:
+
+```javascript
+window.Koliseo = window.Koliseo || {};
+Koliseo.resources = {
+	it: {
+	  ticketsAvailable: 'biglietti disponibili',
+      seeOther: 'vedi altre date',
+      buy: 'Compra',
+      empty: 'Nessuno spettacolo trovato',
+      dateFormat: 'Ddd Mm* d*',
+      weekDays: 'Domenica Lunedì Martedì Mercoledì Giovedì Venerdì Sabato',
+      months: 'Gennaio Febbraio Marzo Aprile Maggio Giugno Luglio Agosto Settembre Ottobre Novembre Dicembre'
+	}
+};
+```
+
+License
+=======
+
+These widgets are released under the (http://en.wikipedia.org/wiki/MIT_License)(MIT) license. You may modify these widgets and include them in your own commercial, non-open-source projects as long as the copyright header is left intact.
--- src/kperf.js
+++ src/kperf.js
@@ -1,129 +1,225 @@
+/*!
+ * Koliseo KPerf Widget v1.0
+ *
+ * Copyright 2011 Nacho Coloma
+ * Licensed under the MIT license.
+ */
+
 /**
-	Displays a widget with a list of performances for a concrete show
-	Requires: jquery, underscore
+  Displays a widget with a list of performances for a concrete show
+  Requires: jquery
+  Options:
+    showId {String} the show ID (required)
+    locale {String} the locale to use, default is the browser locale or "en" if none.
+    dateFormat {String} the date format to use, default is the locale-specific default.
+    maxEntries {number} the maximum number of entries to display, default 5.
+    insertCss {boolean} insert automatically the stylesheet if not already present. Default true.
 */
-(function($) {
+(function($, w) {
 
-	var 
+  /**
+    All locale-dependent entries
+    You can specify new locales before loading this script. E.g.:
+    window.Koliseo = {
+      resources: {
+        fr: {
+          // entries go here
+        }
+      }
+    };
+  */
+  var allResources = $.extend({
+    en: {
+      ticketsAvailable: 'tickets available',
+      seeOther: 'see other dates',
+      buy: 'Buy tickets',
+      empty: 'No performances found',
+      dateFormat: 'Ddd Mm* d*',
+      weekDays: 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday',
+      months: 'January February March April May June July August September October November December'
+    },
+    es: {
+      ticketsAvailable: 'de entradas disponibles',
+      seeOther: 'ver otras fechas',
+      buy: 'Comprar',
+      empty: 'No hay actuaciones disponibles',
+      dateFormat: 'Ddd d* de Mm*',
+      weekDays: 'Domingo Lunes Martes Miércoles Jueves Viernes Sábado',
+      months: 'Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre'
+    }
+  }, w.Koliseo? w.Koliseo.resources : undefined);
 
-	/** the options provided by the user */
-	options,
+  $.fn.kperf = function(opt) {
 
-	/** the jQuery container where the HTML will be inserted */
-	$element,
+    var 
 
-	/** Transform 0-1 into 0-100 */
-	toPercentage = function(value) {
-		return +((value * 100).toFixed(0));
-	},
+      /** the options provided by the user */
+      options,
 
-	/** extract the date from a ISO8601 dateTime */
-	extractDate = function(dateTimeStr) {
-		return (/(.+)T/).exec(dateTimeStr)[1];
-	},
+      /** i18n resources for the current locale */
+      res,
 
-	/** Render the button to buy */
-	renderBuyButton = function() {
-		$element.append('<div class="kbuttonbar"><a class="kbtn kprimary" href="' + options.showURL + '">' + options.res.buy + '</a></div>');
-	},
-	
-	/** Render the provided list of performances and the button to buy */
-	renderPerformances = function(performances) {
-		var 
-			html = '',
-			perfByDateAndVenue = {},
-			entriesCount = 0
-		;
-		$.each(performances, function(i, performance) {
-			id = extractDate(performance.dateTimeStr) + '-' + performance.venue.id;
-			if (!perfByDateAndVenue[id] || perfByDateAndVenue[id].minprice > performance.minprice) {
-				perfByDateAndVenue[id] = performance;
-			} 
-		});
-		for (var id in perfByDateAndVenue) {
-			if (perfByDateAndVenue.hasOwnProperty(id)) {
-				if (entriesCount++ >= options.maxEntries) {
-					break;
-				}
-				var 
-					performance = perfByDateAndVenue[id],
-					availableTickets = performance.totalTickets - performance.soldTickets,
-					parts = /(\d+)-(\d+)-(\d+)/.exec(performance.dateTimeStr),
-					pct = toPercentage(availableTickets / performance.totalTickets),
-					ticketsClass = pct === 0? '' :
-						pct < 20? 'ksoldout' :
-						pct < 40? 'kscarce' :
-						'kplenty'
-					;
-				html += 
-					'<li class="kperformance" title="' + pct + '% ' + options.res.ticketsAvailable + '">' +
-						'<input type="radio" class="kradio" name="kperformance" value="' + performance.id + '">' + 
-							options.dateToString(parts) + ' <b class="kprice">(' + performance.minPrice + ' ' + performance.currency + ')</b>' + 
-							'<span class="ktickets ' + ticketsClass + '" >' + 
-								availableTickets + '/' + performance.totalTickets  +
-							'</span>' + 
-						'<span class="kvenue"><span class="kvenuename">' + performance.venue.name + '</span> <span class="kvenueaddr">' + performance.venue.address + '</span></span>' +
-					'</li>'
-					;
+      /** the widget container */
+      $element = this,
 
-			}
-		}
-		if (!performances.length)
-			html = '<li class="kempty kperformance">' + options.res.empty + '</li>';
-		$element.html('<ul class="kperformances">' + html + '</ul>');
+      /** Transform 0-1 into 0-100 */
+      toPercentage = function(value) {
+        return +((value * 100).toFixed(0));
+      },
 
-		console.log(performances);
-		renderBuyButton();
-		$element.append('<a class="kseeother" href="' + options.showURL + '">&raquo; ' + options.res.seeOther + '</a>');
-	};
+      /** extract the date from a ISO8601 dateTime */
+      extractDate = function(dateTimeStr) {
+        return (/(.+)T/).exec(dateTimeStr)[1];
+      },
 
-	/**
-		options.showId {String} the show ID
-		options.hostname {String} the host name, default www.koliseo.com
-	*/
-	$.fn.perfw = function(opt) {
-		$element = this;
-		if (!opt.showId)
-			throw new Error("options.showId is required");
-		options = $.extend({
-			hostname: 'www.koliseo.com',
-			dateToString: function(date) {
-				return parts[1] + '-' + parts[2] + '-' + parts[3];
-			},
-			maxEntries: 5,
-			res: $.extend({
-				ticketsAvailable: 'tickets available',
-				seeOther: 'see other dates',
-				buy: 'Buy tickets',
-				empty: 'No performances found'
-			}, opt? opt.res : null)
-		}, opt);
-		options.showURL = 'http://' + options.hostname + '/shows/' + options.showId;
+      /** format a date to the current locale */
+      formatDate = function (date, f) {
+        var 
+          nm = res.months.split(' ')[date.getMonth()],
+          nd = res.weekDays.split(' ')[date.getDay()],
+          padLeft = function(str) {
+            str = String(str);
+            var pad = "00";
+            return pad.substring(0, pad.length - str.length) + str;
+          };
 
-		$element.addClass('kcontainer');
-		if ($.support.cors) {
-			$.ajax({
-				url: options.showURL + '/performances',
-				dataType: 'json',
-				type: 'GET',
-				success: function(performances) {
-					renderPerformances(performances);
-				},
-				error: function(jqXHR, errorThrown) {
-					console.error(errorThrown);
-					renderBuyButton();
-				}
-			});
-		} else {
-			renderBuyButton();
-		}
-		$element.on('click', '.kbtn', function(e) {
-			var $radio = $element.find('.kradio:checked');
-			if ($radio.length) {
-				window.location.href = options.showURL + "/performance?performance=" + $radio.val();
-				return false;
-			}
-		});
-	};
+        return f
+          .replace(/yyyy/g, date.getFullYear())
+          .replace(/yy/g, String(date.getFullYear()).substr(2,2))
+          .replace(/MMM/g, nm.substr(0,3).toUpperCase())
+          .replace(/Mmm/g, nm.substr(0,3))
+          .replace(/MM\*/g, nm.toUpperCase())
+          .replace(/Mm\*/g, nm)
+          .replace(/mm/g, padLeft(date.getMonth()+1))
+          .replace(/DDD/g, nd.substr(0,3).toUpperCase())
+          .replace(/Ddd/g, nd.substr(0,3))
+          .replace(/DD\*/g, nd.toUpperCase())
+          .replace(/Dd\*/g, nd)
+          .replace(/dd/g, padLeft(date.getDate()))
+          .replace(/d\*/g, date.getDate());
+      },
 
-})(jQuery);
+      /** Render the "buy" button */
+      renderBuyButton = function() {
+        $element.append('<div class="kbuttonbar"><a class="kbtn kprimary" href="' + options.showURL + '">' + res.buy + '</a></div>');
+      },
+      
+      /** Render the provided list of performances */
+      renderPerformances = function(performances) {
+        var 
+          html = '',
+          perfByDateAndVenue = {},
+          entriesCount = 0
+        ;
+        $.each(performances, function(i, performance) {
+          id = extractDate(performance.dateTimeStr) + '-' + performance.venue.id;
+          if (!perfByDateAndVenue[id] || perfByDateAndVenue[id].minprice > performance.minprice) {
+            perfByDateAndVenue[id] = performance;
+          } 
+        });
+        for (var id in perfByDateAndVenue) {
+          if (perfByDateAndVenue.hasOwnProperty(id)) {
+            if (entriesCount++ >= options.maxEntries) {
+              break;
+            }
+            var 
+              performance = perfByDateAndVenue[id],
+              availableTickets = performance.totalTickets - performance.soldTickets,
+              parts = /(\d+)-(\d+)-(\d+)/.exec(performance.dateTimeStr),
+              pct = toPercentage(availableTickets / performance.totalTickets),
+              ticketsClass = pct === 0? '' :
+                pct < 20? 'ksoldout' :
+                pct < 40? 'kscarce' :
+                'kplenty'
+              ;
+            html += 
+              '<li class="kperformance" title="' + pct + '% ' + res.ticketsAvailable + '">' +
+                '<input type="radio" class="kradio" name="kperformance" value="' + performance.id + '">' + 
+                  formatDate(new Date(parts[1], parts[2], parts[3]), options.dateFormat) + ' <b class="kprice">(' + performance.minPrice + ' ' + performance.currency + ')</b>' + 
+                  '<span class="ktickets ' + ticketsClass + '" >' + 
+                    availableTickets + '/' + performance.totalTickets  +
+                  '</span>' + 
+                '<span class="kvenue"><span class="kvenuename">' + performance.venue.name + '</span> <span class="kvenueaddr">' + performance.venue.address + '</span></span>' +
+              '</li>'
+              ;
+
+          }
+        }
+        html = html || '<li class="kempty kperformance">' + res.empty + '</li>';
+        $element.append('<ul class="kperformances">' + html + '</ul>');
+
+        renderBuyButton();
+        $element.append('<a class="kseeother" href="' + options.showURL + '">&raquo; ' + res.seeOther + '</a>');
+      },
+
+      /** insert kperf.css if not already present */
+      insertStylesheet = function() {
+        var url = $('script[src*="/kperf"]').attr('src');
+        if (url) {
+          url = url.replace('.js', '.css');
+          $('link[href="' + url + '"]').length || $('head').append('<link rel="stylesheet" href="' + url + '">');
+        }
+      };
+
+    if (!this.length)
+      return;
+
+    // initialize options with default values
+    options = $.extend({
+      hostname: 'www.koliseo.com',
+      maxEntries: 5,
+      insertCss: true
+    }, this.data(), opt);
+
+    if (!options.showId)
+      throw new Error("options.showId is required");
+    options.showURL = 'http://' + options.hostname + '/shows/' + options.showId;
+
+    // initialize CSS
+    options.insertCss && insertStylesheet();
+
+    // initialize res
+    var locale = options.locale;
+    if (!locale) {
+      var parts = /([^\-]{2})-.+/.exec(navigator.language || navigator.userLanguage || '');
+      parts && (locale = parts[1]);
+    }
+    res = options.res || allResources[locale] || allResources.en;
+    options.dateFormat = options.dateFormat || res.dateFormat;
+
+    // get performances and render
+    $element.addClass('kcontainer').html('');
+    if ($.support.cors) {
+      $element.addClass('kloading');
+      $.ajax({
+        url: options.showURL + '/performances',
+        dataType: 'json',
+        type: 'GET',
+        success: function(performances) {
+          $element.removeClass('kloading');
+          renderPerformances(performances);
+        },
+        error: function(jqXHR, errorThrown) {
+          $element.removeClass('kloading');
+          console.error(errorThrown);
+          renderBuyButton();
+        }
+      });
+    } else {
+      renderBuyButton();
+    }
+
+    // when user clicks the "buy" button, go to the selected performance or to the show page
+    $element.on('click', '.kbtn', function(e) {
+      var $radio = $element.find('.kradio:checked');
+      if ($radio.length) {
+        window.location.href = options.showURL + "/performance?performance=" + $radio.val();
+        return false;
+      }
+    });
+  };
+
+  // initialize any .kcontainer element
+  $('.kcontainer').kperf();
+
+})(jQuery, window);
